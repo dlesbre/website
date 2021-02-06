e101b18bf6ae8f458de5da3955760181eb3e6647
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-02-06 13:22:42 +0100

    Fix theme switch

--- www/js/script.js
+++ www/js/script.js
@@ -1,12 +1,33 @@
-var is_dark = true;
+const storage = localStorage;
+const theme_key = "theme";
+const theme_light = "light";
+
+function get_current_theme() {
+	console.log(storage);
+	const theme = storage.getItem(theme_key);
+	if (theme == theme_light)
+		return false;
+	return true;
+}
 
 function toggle_theme() {
-	var button = document.getElementsByClassName("themetoggle");
-	document.body.classList.toggle("light-theme");
-	is_dark = !is_dark;
-	button.title = is_dark ? "Thème clair" : "Thème sombre";
-	console.log("toggle");
-	console.log(is_dark);
-	console.log(button.title);
-	console.log(is_dark ? "Thème clair" : "Thème sombre");
+	var theme = !get_current_theme();
+	set_theme(theme);
 }
+
+function set_theme(dark) {
+	const theme_class = "light-theme";
+	if (dark) {
+		document.body.classList.remove(theme_class);
+		storage.removeItem(theme_key);
+	}
+	else {
+		document.body.classList.add(theme_class);
+		storage.setItem(theme_key, theme_light);
+	}
+	console.log(storage);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+	set_theme(get_current_theme());
+});
