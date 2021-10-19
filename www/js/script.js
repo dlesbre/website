6547cdd90bf187e779595362e06d4fa169335945
Dorian Lesbre <dorian.lesbre@gmail.com>
2021-10-19 15:24:46 +0200

    Update JS

--- www/js/script.js
+++ www/js/script.js
@@ -1,33 +1,48 @@
-const storage = localStorage;
-const theme_key = "theme";
-const theme_light = "light";
+const STORAGE = localStorage;
+// Key used to store theme in local storage
+const THEME_KEY = "theme";
+// Class to add to html body to switch themes (defined in the CSS)
+const THEME_CLASS = "light-theme";
 
+// Possible themes
+const THEME_ENUM = {
+	Light: "light",
+	Dark : "dark",
+};
+
+// Get the theme in local storage
+// If not present, return the default (dark) theme
 function get_current_theme() {
-	console.log(storage);
-	const theme = storage.getItem(theme_key);
-	if (theme == theme_light)
-		return false;
-	return true;
+	const theme = STORAGE.getItem(THEME_KEY);
+	if (theme == THEME_ENUM.Light)
+		return THEME_ENUM.Light;
+	return THEME_ENUM.Dark; // default theme
 }
 
+// Called when clicking the toggle button
 function toggle_theme() {
-	var theme = !get_current_theme();
-	set_theme(theme);
+	const theme = get_current_theme();
+	if (theme == THEME_ENUM.Light)
+		set_theme(THEME_ENUM.Dark);
+	else
+		set_theme(THEME_ENUM.Light);
 }
 
-function set_theme(dark) {
-	const theme_class = "light-theme";
-	if (dark) {
-		document.body.classList.remove(theme_class);
-		storage.removeItem(theme_key);
+// Set the them to the given value
+function set_theme(theme) {
+	if (theme == THEME_ENUM.Dark) {
+		// Remove THEME_CLASS from html body for default theme
+		// Also remove it from local storage
+		document.body.classList.remove(THEME_CLASS);
+		STORAGE.removeItem(THEME_KEY);
 	}
 	else {
-		document.body.classList.add(theme_class);
-		storage.setItem(theme_key, theme_light);
+		document.body.classList.add(THEME_CLASS);
+		STORAGE.setItem(THEME_KEY, THEME_ENUM.Light);
 	}
-	console.log(storage);
 }
 
+// Set the theme on document load to be consistent across multiple pages
 document.addEventListener('DOMContentLoaded', function () {
 	set_theme(get_current_theme());
 });
