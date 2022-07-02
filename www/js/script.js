5394cc4318c3c654f50a264676674f182b69c606
Dorian Lesbre <dorian.lesbre@gmail.com>
2022-07-02 19:51:13 +0200

    Fix theme script for small pages

--- www/js/script.js
+++ www/js/script.js
@@ -1,8 +1,9 @@
+// @ts-check
 // ==================================================
 // Light/Dark theme
 // ==================================================
 
-const STORAGE = localStorage;
+const STORAGE = sessionStorage;
 // Key used to store theme in local storage
 const THEME_KEY = "theme";
 // Class to add to html body to switch themes (defined in the CSS)
@@ -34,14 +35,15 @@ function toggle_theme() {
 
 // Set the them to the given value
 function set_theme(theme) {
+	const html = document.documentElement;
 	if (theme == THEME_ENUM.Dark) {
 		// Remove THEME_CLASS from html body for default theme
 		// Also remove it from local storage
-		document.body.classList.remove(THEME_CLASS);
+		html.classList.remove(THEME_CLASS);
 		STORAGE.removeItem(THEME_KEY);
 	}
 	else {
-		document.body.classList.add(THEME_CLASS);
+		html.classList.add(THEME_CLASS);
 		STORAGE.setItem(THEME_KEY, THEME_ENUM.Light);
 	}
 }
