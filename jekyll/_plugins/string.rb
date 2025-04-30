0719346a04e78d69c53a796f9817a1aaeb0de3d6
Dorian Lesbre <dorian.lesbre@gmail.com>
2025-04-30 23:40:24 +0200

    Update

--- jekyll/_plugins/string.rb
+++ jekyll/_plugins/string.rb
@@ -1,25 +1,38 @@
 # Various string manipulation filter that liquid sadly does not have
 module StringFilters
+  # Check if text has the given prefix
   def has_prefix(text, prefix)
     text.start_with? prefix
   end
 
+  # Check if text has the given suffix
   def has_suffix(text, suffix)
     text.end_with? suffix
   end
 
-  def strip_extension(text)
-    text.reverse().sub(/^.+?\./, '').reverse()
+  # Remove extension (last .XXX of path)
+  # index -> index
+  # index.html -> index
+  # index.html.fr -> index.html
+  def strip_extension(path)
+    # We need to flip because lazy regex matches dont work well with end conditions
+    # I.E. /\..+?$/ matches ".html.fr" in "index.html.fr", and not just ".fr"
+    path.reverse().sub(/^.+?\./, '').reverse()
   end
 
-  def extension(text)
-    if text.include? '.' then
-      text.sub(/^.*\./, '')
+  # Return extension of path, without leading dot
+  # index -> ""
+  # index.html -> html
+  # index.html.fr -> fr
+  def extension(path)
+    if path.include? '.' then
+      path.sub(/^.*\./, '')
     else
-      text
+      ""
     end
   end
 
+  # If the text has the given prefix, remove it
   def strip_prefix(text, prefix)
     if has_prefix(text, prefix) then
       text[prefix.length..]
@@ -28,6 +41,7 @@ module StringFilters
     end
   end
 
+  # If the text has the given suffix, remove it
   def strip_suffix(text, suffix)
     if has_suffix(text, suffix) then
       text[..-suffix.length]
