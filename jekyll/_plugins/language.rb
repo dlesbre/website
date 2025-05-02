06ffd116ee0b0bd3d725259566d8786d653ed1cb
Dorian Lesbre <dorian.lesbre@gmail.com>
2025-05-02 23:43:42 +0200

    Alt lang plugin

---
+++ jekyll/_plugins/language.rb
@@ -0,0 +1,55 @@
+# Generate alternative language pages
+# I.E. does nothing for pages with permalink ending in .html.fr or .html.en
+# But for pages with permalink ending in .html:
+# - Change it to .html.<page.lang> (or .fr if no page.lang)
+# - Create a copy for the other language (so .en if page.lang=fr), adding a disclaimer
+#   "this page is only available in french" at the top of the content.
+module Jekyll
+  class Page
+    def url=(permalink)
+      @url = URL.new(
+        :template     => template,
+        :placeholders => url_placeholders,
+        :permalink    => permalink
+      ).to_s
+
+    end
+  end
+end
+
+module LanguageAlternatePagePlugin
+  class LanguageAlternateGenerator < Jekyll::Generator
+    def generate(site)
+      alt_pages = []
+      site.pages.each do |page|
+        if page.url.end_with? '.html' then
+          alt_pages << page
+        end
+      end
+      alt_pages.each do |page|
+        site.pages.delete(page)
+        puts page.dir + " " + page.basename + page.name
+        site.pages << LanguageAlternatePage.new(site, page, "fr")
+        site.pages << LanguageAlternatePage.new(site, page, "en")
+      end
+    end
+  end
+
+  class LanguageAlternatePage < Jekyll::Page
+    def initialize(site, page, lang)
+      @site = site
+      @base = site.source
+      @dir = page.dir
+      @basename = page.basename
+      @ext = page.ext
+      @name = @basename + @ext
+      @data = page.data.dup()
+      @content = page.content
+      if lang != @data["lang"] then
+        @data["lang-disclaimer"] = true
+      end
+      @data["lang"] = lang
+      @data["permalink"] = page.url + '.' + lang
+    end
+  end
+end
